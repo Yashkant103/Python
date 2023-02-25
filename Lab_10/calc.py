ed53ed232bf971861cd3c49f516dd2f83570f799
Yashkant103 <100015305+Yashkant103@users.noreply.github.com>
2023-02-25 16:37:16 +0530

    Updates

---
+++ Lab_10/calc.py
@@ -0,0 +1,11 @@
+def add(n1,n2):
+    print("Sum is ",n1+n2)
+    
+def sub(n1,n2):
+    print("Difference is ",n1-n2)
+    
+def mul(n1,n2):
+    print("Product is ",n1*n2)
+    
+def div(n1,n2):
+    print("Division is ",n1/n2)
